38fd06b954cc92bc5259d719cecc3ee1ce2ee035
Stefan <stefan@berliner-gartenwetter.de>
2021-12-07 02:52:10 +0100

    Tag6

--- 2021/2021-12-06/tag5.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-with open("t5input","r") as file:
-    vent_lines = [[complex(*map(int,values.split(","))) for values in line.strip().split(" -> ") ]  for line in file.readlines() ]
