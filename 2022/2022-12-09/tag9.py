97d673bf20bd46d87c7f29626ad126ac3bc928ee
Stefan <stefan@berliner-gartenwetter.de>
2022-12-10 03:22:27 +0100

    Tag9

---
+++ 2022/2022-12-09/tag9.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  9 22:36:31 2022
+
+@author: frendor
+"""
+
+from operator import itemgetter
+
+with open("example2","r") as infile:
+    moves = [(d,int(v)) for line in infile.read().strip().split("\n") for d,v in [line.split()]]
+
+def print_field(positions):
+    x_vals = [int(v.real) for v in positions]
+    y_vals = [int(v.imag) for v in positions]
+    xmin, xmax = min(x_vals)-1, max(x_vals)+1
+    ymin, ymax = min(y_vals)-1, max(y_vals)+1
+    
+    for y in range(ymin, ymax):
+        print( "".join(["s" if x==0 and y == 0 else "#" if complex(x,y) in positions else "."  for x in range(xmin,xmax+1)]) )
+
+directions = {'R':1,
+              'L':-1,
+              'U':-1j,
+              'D':1j}
+
+distance = lambda p1, p2: (p1-p2).real**2 + (p1-p2).imag**2 
+
+def rope_walker(part):
+    position = {nr:0 for nr in range(10)}
+    visited_positions = {nr:set([0]) for nr in range(10)}
+    
+    part_factor = {"Part1":1,
+                   "Part2":9}[part]
+    
+    active_knots = list(range(1+part_factor))
+    
+    for d,v in moves:
+        for steps in range(v):
+            position[0] += directions[d]
+            
+            visited_positions[0].add(position[0])
+            
+            for k_lead,k_follow in zip(active_knots,active_knots[1:]):
+                tails_diff = position[k_lead] - position[k_follow]
+                if abs(tails_diff.real) > 1 or abs(tails_diff.imag)>1:
+                    
+                    position[k_follow] = sorted([ (distance(position[k_lead], position[k_follow] + complex(dx,dy)), position[k_follow] +complex(dx,dy))  for dx in [-1,0,1] for dy in [-1,0,1] ],key=itemgetter(0))[0][1]
+                    
+                    visited_positions[k_follow].add(position[k_follow])
+    return visited_positions[part_factor]
+      
+for part in ["Part1","Part2"]:
+    path = rope_walker(part)    
+    print(f"{part}: ", len(path))
+    print_field(path)
